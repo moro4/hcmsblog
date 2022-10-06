61d04c5e7a8e30db65422b917aeb42b0937a0537
zepeersGily <>
2022-10-06 12:52:38 +0200

    implemented author bio section on the post page

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,7 +1,23 @@
-function Author() {
+import Image from 'next/image';
+
+function Author({author}) {
    return (
-      <div>
-         <h1>Author</h1>
+      <div className='text-center mt-20 mb-8 p-12 relative rounded-lg
+         bg-black bg-opacity-20'
+      >
+         <div className='absolute left-0 right-0 -top-12 block'>
+            <Image src={author.photo.url} alt={author.name} layout='intrinsic'
+               height='100px'width='100px' unoptimized className='align-middle
+               rounded-full object-cover'
+            />
+         </div>
+
+         <h3 className='text-white my-4 text-xl font-bold'>
+            {author.name}
+         </h3>
+
+         <p className='text-white text-lg'>{author.bio}</p>
+
       </div>
    )
 }
