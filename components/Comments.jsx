77679a7548a3d39b9303595bd3685163590fbed8
zepeersGily <>
2022-10-11 09:09:47 +0200

    implemented comments section on the blog post page

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,8 +1,40 @@
-function Comments() {
+import {useState, useEffect} from 'react';
+import moment from 'moment';
+import parse from 'html-react-parser';
+import {getComments} from '../services';
+
+function Comments({slug}) {
+
+   const [comments, setComments] = useState([]);
+
+   useEffect(() => {
+      getComments(slug).then(result => setComments(result));
+   }, []);
+
    return (
-      <div>
-         <h1>Comments</h1>
-      </div>
+      <>
+         {comments.length > 0 && (
+            <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
+               <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+                  {comments.length + ' '} Comments
+               </h3>
+               {comments.map((comment) => (
+                  <div key={comment.createdAt} className='border-b mb-4 pb-4
+                     border-gray-100'
+                  >
+                     <p className='mb-4'>
+                        <span className='font-semibold'>{comment.name}</span>
+                        &nbsp; on &nbsp;
+                        {moment(comment.createdAt).format('DD MMM, YYYY')}
+                     </p>
+                     <p className='whitespace-pre-line text-gray-600 w-full'>
+                        {parse(comment.comment)}
+                     </p>
+                  </div>
+               ))}
+            </div>
+         )}
+      </>
    )
 }
 
