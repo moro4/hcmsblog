ef69d71c53531fc0a803b0001abcf6056e977e67
zepeersGily <>
2022-10-11 10:10:31 +0200

    implemented category routing

---
+++ pages/category/[slug].js
@@ -0,0 +1,37 @@
+import { getCategories, getCategoryPost } from '../../services';
+import { PostCard, Categories } from '../../components';
+
+export default function CategoryPost({ posts }) {
+   return (
+      <div className="container mx-auto px-10 mb-8">
+         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+            <div className="col-span-1 lg:col-span-8">
+               {posts.map((post, index) => (
+                  <PostCard key={index} post={post} />
+               ))}
+            </div>
+            <div className="col-span-1 lg:col-span-4">
+               <div className="relative lg:sticky top-8">
+                  <Categories />
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export async function getStaticProps({ params }) {
+   const posts = await getCategoryPost(params.slug);
+
+   return {
+      props: { posts },
+   };
+}
+
+export async function getStaticPaths() {
+   const categories = await getCategories();
+   return {
+      paths: categories.map(({ slug }) => ({ params: { slug } })),
+      fallback: true,
+   };
+}
--- services/index.js
+++ services/index.js
@@ -129,6 +129,42 @@ export async function getCategories() {
    return result.categories;
 }
 
+export async function getCategoryPost(slug) {
+   const query = gql`
+     query GetCategoryPost($slug: String!) {
+       postsConnection(where: {categories_some: {slug: $slug}}) {
+         edges {
+           cursor
+           node {
+             author {
+               bio
+               name
+               id
+               photo {
+                 url
+               }
+             }
+             createdAt
+             slug
+             title
+             excerpt
+             featuredImage {
+               url
+             }
+             categories {
+               name
+               slug
+             }
+           }
+         }
+       }
+     }
+   `;
+
+   const result = await request(graphqlAPI, query, { slug });
+   return result.postsConnection.edges;
+};
+
 export async function submitComment(obj) {
    const result = await fetch('/api/comments', {
       method: 'POST',
