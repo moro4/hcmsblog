e13f968a8e118844d9930e6d09a8504229fe81f5
zepeersGily <>
2022-10-11 12:43:53 +0200

    implemented a mandatory fallback page to make SSG work

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,7 +1,15 @@
 import { getCategories, getCategoryPost } from '../../services';
-import { PostCard, Categories } from '../../components';
+import { PostCard, Categories, Loader } from '../../components';
+import { useRouter } from 'next/router';
 
 export default function CategoryPost({ posts }) {
+
+   const router = useRouter();
+
+   if (router.isFallback) {
+      return <Loader />;
+   }
+
    return (
       <div className="container mx-auto px-10 mb-8">
          <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
